c321763c6e8389810b27f9038ff278e28ad8de97
aintelligen <aintelligen@163.com>
2019-05-03 15:08:26 +0800

    Feat：complete  10

--- 5/metrics.py
+++ 5/metrics.py
@@ -29,3 +29,90 @@ def mean_absolute_error(y_true, y_predict):
 
 def r2_score(y_true, y_predict):
     return 1 - mean_squared_error(y_true, y_predict) / np.var(y_true)
+
+
+def TN(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 0) & (y_predict == 0))
+
+
+def FP(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 0) & (y_predict == 1))
+
+
+def FN(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 1) & (y_predict == 0))
+
+
+def TP(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 1) & (y_predict == 1))
+
+
+def confusion_matrix(y_true, y_predict):
+    return np.array([
+        [TN(y_test, y_log_predict), FP(y_test, y_log_predict)],
+        [FN(y_test, y_log_predict), TP(y_test, y_log_predict)],
+    ])
+
+
+def precision_score(y_true, y_predict):
+    tp = TP(y_true, y_predict)
+    fp = FP(y_true, y_predict)
+    try:
+        return tp / (tp+fp)
+    except:
+        return 0.0
+
+
+def recall_score(y_true, y_predict):
+    tp = TP(y_true, y_predict)
+    fn = FN(y_true, y_predict)
+    try:
+        return tp / (tp+fn)
+    except:
+        return 0.0
+
+
+def plot_decision_boundary(model, axis):
+    x0, x1 = np.meshgrid(
+        np.linspace(axis[0], axis[1], int(
+            (axis[1] - axis[0]) * 100)).reshape(10, -1),
+        np.linspace(axis[2], axis[3], int(
+            (axis[3] - axis[2]) * 100)).reshape(10, -1)
+    )
+    X_new = np.c_[x0.ravel(), x1.ravel()]
+
+    y_predict = model.predict(X_new)
+    zz = y_predict.reshape(x0.shape)
+
+    from matplotlib.colors import ListedColormap
+    custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
+    plt.contourf(x0, x1, zz, cmap=custom_cmap)
+
+
+def F1_score(precision, recall):
+    try:
+        return 2*precision*recall / (precision+recall)
+    except:
+        return 0.0
+
+
+def TPR(y_true, y_predict):
+    tp = TP(y_true, y_predict)
+    fn = FN(y_true, y_predict)
+    try:
+        return tp / (tp+fn)
+    except:
+        return 0.0
+
+
+def FPR(y_true, y_predict):
+    tn = TN(y_true, y_predict)
+    fp = FP(y_true, y_predict)
+    try:
+        return fp / (tn+fp)
+    except:
+        return 0.0
